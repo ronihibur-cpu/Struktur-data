9fb41f0a2e41749fac8f1ec761c7a76942cf03c1
ronihibur-cpu <ronihibur@gmail.com>
2026-04-06 18:53:18 +0800

    Add files via upload

---
+++ kondisi.py
@@ -0,0 +1,17 @@
+nama=input("MASUKKAN NAMA PENDEK ANDA :")
+if nama == "Roni":
+    print("selamat datang Roni")
+else:
+    print("program selesai")
+    
+umur=int(input("MASUKAN UMUR ANDA :"))
+#kondisi
+if umur <=0:
+    print("Anda belum lahir")
+elif umur>60:
+    print("banyakin ibadah bentar lagi mati")
+elif umur>=18:
+    print("Anda cukup umur")
+else:
+    print("anda belum cukup umur")
+    print("program selesai")
