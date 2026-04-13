a7286fa5dea749449ea3772c623d1c4b2f14c1ea
ronihibur-cpu <ronihibur@gmail.com>
2026-04-13 19:13:02 +0800

    Add files via upload

---
+++ tugas_2.py
@@ -0,0 +1,14 @@
+#bagian 1 :verivikasi nama
+nama=input("masukan nama pendek anda :")
+
+if nama =="Roni":
+    print("Lanjut ke program selanjutnya")
+else :
+    print("Silahkan coba lagi !")
+    
+
+#bagian 2 : tabel perkalian 
+angka =int(input("masukan angka :"))
+for i in range(1,11):
+    hasil=angka*i
+    print(f"{angka}x{i}={hasil}")
